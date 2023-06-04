9eade89bac9466e71103dbfa597a7acb934b719c
Doroga80 <rusdorog@yandex.by>
2023-06-04 16:34:04 +0300

    main

---
+++ Main.java
@@ -0,0 +1,22 @@
+import static task.task1.divide;
+import static task.task1.getValue;
+import static task.task1.createArray;
+import static task.task3.diffArray;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // задача 1.1
+        divide(15, 3);
+
+        // задача 1.2
+        getValue(new int[20], 35);
+
+        // задача 1.3
+        createArray(-12);
+
+        // задача 3.0
+        diffArray(new int[15], new int[18]);
+    }
+    
+}
